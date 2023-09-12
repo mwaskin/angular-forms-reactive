59c5c66648eeb1e3b174a264b9635edd33a65890
Matthew Waskin <matthew@waskin.com>
2023-09-12 16:21:05 -0400

    add 'Add Hobbies' button to allow use to add any hobbies to the 'hobbies' FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -29,7 +29,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 -- The validators are methods on the Validators class, imported from angular
 -- The method for each validator is not called when passed; it's only referenced for Angular to call behind the scenes when checking the validation
 */
-/* ACCESSING FORMCONTROL DATA
+/* ACCESSING FormControl DATA
 -- Use FormGroup.get() method
 -- Pass (as a string) the name of the FormControl to access
 -- Then access the properties of that control (.valid, .touched, etc.)
@@ -39,6 +39,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 -- group related controls by nesting a child FormGroup inside the main parent FormGroup
 -- In template, wrap the grouped controls in a container and bind the formGroupName directive, passing it the name of the child FormGroup
 -- Now the control is accessed by calling parentFG.childFG; for example, signupForm.get('userData.username')
+*/
+/* CREATING/USING AN ARRAY OF FormControls (FormArray)
+-- Add a new control to FormGroup of type FormArray(), which takes an array of controls as an argument
+-- In the template, wrap the section of the form with the FormArray in a container and sync it to the FormArray by binding the formArrayName directive
+-- onAddHobby():
+  -- when button is clicked, a new null control is created
+  -- use FormGroup.get() method to receive the FormArray; must manually type-cast the result as a <FormArray>
+    -- call .push() method to add the new control to the array
+-- getControls()
+  -- again, use .get() method and type cast it as a <FormArray> to get the hobbies FormArray object
+  -- access the controls property on the FormArray object to get the array on controls (added by each button click)
+-- Rendering and Syncing the Controls
+  -- use *ngFor to render a div containing an input for each control added to the hobbies FormArray
+  -- sync each input to its corresponding control in the FormArray by syncing its formControlName to it's index in the FormArray
+    -- use property binding to bind it to be able to assign a variable (i) rather than a string (static) value
 */
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
@@ -51,10 +66,20 @@ export class AppComponent implements OnInit {
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([]),
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
   }
 }
