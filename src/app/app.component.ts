281a57676f4377ef4f40d0e6479367b9b51908c9
Matthew Waskin <matthew@waskin.com>
2023-09-12 12:23:22 -0400

    add validators to FormControls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -23,14 +23,20 @@ import { FormControl, FormGroup } from '@angular/forms';
 -- Bind (ngSubmit) event to <form> and pass submission handler method
 -- Difference is that the form was defined in the component's TS class, so don't need to create and pass a local ref for the form to the handler
 */
+/* VALIDATION
+-- Bc it's not the TD approach, validators are not added as directives bound to the HTML elements
+-- Validators array is added as an argument when initializing each FormControl (no array needed if just one Validator)
+-- The validators are methods on the Validators class, imported from angular
+-- The method for each validator is not called when passed; it's only referenced for Angular to call behind the scenes when checking the validation
+*/
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      username: new FormControl(null),
-      email: new FormControl(null),
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       gender: new FormControl('male'),
     });
   }
